6a9c78dd495e7d5568ec4397b5a2305b8c1a5d5a
Matias129 <117055574+Matias129@users.noreply.github.com>
2022-12-18 22:49:32 -0300

    repositorio final

---
+++ proyectofinal/Proyecto_final/proyectofinal/appblog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from appblog.models import Post
+from appblog.models import Avatar
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Avatar)
